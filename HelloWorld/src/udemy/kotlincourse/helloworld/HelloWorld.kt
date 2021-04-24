511ab75ddf18aa03bb590eb7a16459a1d96a7271
David <david.espinosa.21@gmail.com>
2021-04-24 14:48:36 -0500

    Added a dummy file and updated HelloWorld.kt

--- HelloWorld/src/udemy/kotlincourse/helloworld/HelloWorld.kt
+++ HelloWorld/src/udemy/kotlincourse/helloworld/HelloWorld.kt
@@ -5,10 +5,11 @@ package udemy.kotlincourse.helloworld
 // The class will be generated for us under-the-cover (Kotlin is meant to be more concise)
 fun main(args: Array<String>) {
     // the A in Array is capitalized, everything is a class in Kotlin
+    // Array is found in package kotlin, which is imported automatically
     // there is no array (lowercase) in Kotlin
     // also no return type for this function
 
     // Hello world!
     // no semi-colons in Kotlin
-    println("Hello World!")
+    println("Hello World!")  // println() is found on package kotlin.io, which is imported automatically
 }
