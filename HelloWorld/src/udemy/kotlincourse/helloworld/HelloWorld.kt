5eae222a336dc2d1edf8ecd18e294888c4a07f5a
David <david.espinosa.21@gmail.com>
2021-04-24 14:31:19 -0500

    First commit. Adding a HelloWorld folder.

---
+++ HelloWorld/src/udemy/kotlincourse/helloworld/HelloWorld.kt
@@ -0,0 +1,14 @@
+package udemy.kotlincourse.helloworld
+
+// This function isn't within a class
+// functions declared outside classes are called "top-level functions"
+// The class will be generated for us under-the-cover (Kotlin is meant to be more concise)
+fun main(args: Array<String>) {
+    // the A in Array is capitalized, everything is a class in Kotlin
+    // there is no array (lowercase) in Kotlin
+    // also no return type for this function
+
+    // Hello world!
+    // no semi-colons in Kotlin
+    println("Hello World!")
+}
