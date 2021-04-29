9c2d2c32321566044238380c5994e8d9fd281e9c
David <david.espinosa.21@gmail.com>
2021-04-28 20:16:53 -0500

    Finished video 37. Custom getters and setters in Kotlin

--- KotlinCode/src/udemy/kotlincourse/section05/AccessModifiers2.kt
+++ KotlinCode/src/udemy/kotlincourse/section05/AccessModifiers2.kt
@@ -1,8 +1,8 @@
-// Video 36
+// Video 37
 
 package udemy.kotlincourse.section05
 
-// top-level items are PUBLIC by default
+// top-level items are PUBLIC by default (everything can access)
 // in Java, default is 'package private' (everything in the package can access)
 fun main(args: Array<String>) {
     // four visibility modifiers in Kotlin
@@ -12,7 +12,9 @@ fun main(args: Array<String>) {
     // something marked as 'internal' can be seen from anywhere in the same module
 
     val emp = Employee("Buck")
+    emp.fullTime = false
     println(emp.firstName)
+    println(emp.fullTime)
 
     val emp2 = Employee("Samantha")
     println(emp2.firstName)
@@ -36,7 +38,20 @@ private class Example {
 
 // this class has a 'primary constructor' since it is defined
 // in the same line as the class name
-class Employee(val firstName: String, var fullTime: Boolean = true) {
+class Employee(val firstName: String, fullTime: Boolean = true) {
+
+    // how to write custom getters and setters:
+    var fullTime = fullTime
+    // Code like "employee.fullTime" will invoke this method
+    get() {
+        println("Running the custom get")
+        return field  // field refers to the property its defining a get for - 'fullTime' in this case
+    }
+    // Code like "employee.fullTime = false" will invoke this method
+    set(value) {
+        println("Running the custom set")
+        field = value
+    }
 }
 
 
