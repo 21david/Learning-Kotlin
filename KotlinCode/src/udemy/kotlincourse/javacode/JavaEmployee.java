2ede770590f32c827cb0f42444da60cc7624b11d
David <david.espinosa.21@gmail.com>
2021-04-28 19:58:43 -0500

    Finished video 36. Access modifiers, constructors (primary and secondary), init block, default values

---
+++ KotlinCode/src/udemy/kotlincourse/javacode/JavaEmployee.java
@@ -0,0 +1,12 @@
+package udemy.kotlincourse.javacode;
+
+public class JavaEmployee {
+
+    private final String firstName;
+
+    public JavaEmployee(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+}
---
+++ KotlinCode/src/udemy/kotlincourse/section05/AccessModifiers.kt
@@ -0,0 +1,47 @@
+package udemy.kotlincourse.section05
+
+// top-level items are PUBLIC by default
+// in Java, default is 'package private' (everything in the package can access)
+fun main(args: Array<String>) {
+    // four visibility modifiers in Kotlin
+    // public, private, protected, and internal
+
+    // internal is for modules
+    // something marked as 'internal' can be seen from anywhere in the same module
+
+    val emp = Employee("Buck")
+    println(emp.firstName)
+
+    val emp2 = Employee("Samantha")
+    println(emp2.firstName)
+    println(emp2.fullTime)
+
+    val emp3 = Employee("Jane", false)
+    println(emp3.firstName)
+    println(emp3.fullTime)
+
+}
+
+// no matching requirement between file name and class names
+// so you can have as many public classes as you want in Kotlin
+
+// in Kotlin, you can have a private class (unlike in Java)
+
+// this class is visible ONLY to this file
+private class Example {
+
+}
+
+// this class has a 'primary constructor' since it is defined
+// in the same line as the class name
+class Employee(val firstName: String, var fullTime: Boolean = true) {
+}
+
+
+class Demo {
+    val dummy: String
+
+    constructor() {
+        dummy = "Hello"
+    }
+}
