618af4e99861d83ab32d487f34807d7ab3eafac1
David <david.espinosa.21@gmail.com>
2021-05-05 20:11:20 -0500

    Finished video 77: reading binary files and try with resources (there is no try with resources)

--- KotlinCode/src/udemy/kotlincourse/section08/BinaryIO.kt
+++ KotlinCode/src/udemy/kotlincourse/section08/BinaryIO.kt
@@ -1,5 +1,30 @@
+// Video 77
+
 package udemy.kotlincourse.section08
+
+import java.io.DataInputStream
+import java.io.EOFException
+import java.io.FileInputStream
 
 fun main(args: Array<String>) {
 
+    val di = DataInputStream(FileInputStream("testfile.bin"))
+    var si: String
+
+    try {
+        while(true) {
+            si = di.readUTF()
+            println(si)
+        }
+    }
+    catch(e: EOFException) {}
+
+
+    // try with resources takes care of closing a resource for us
+    // resources have to implement the "Closable" interface to work with it
+    // the way this is done in Kotlin, though, is by using functions with 'use' in them
+    // since they automatically close the resources when done with them
+
 }
+
+// writing files is mostly the same in Kotlin as in Java
